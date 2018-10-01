5052629fda747000e66bb0cd477d80e28255c274
dmitryck <dmitryck@gmail.com>
2018-10-01 23:45:52 +0300

    Mind: Update Mind (add requires, extend by Pack::Ping too);

--- lib/mind.rb
+++ lib/mind.rb
@@ -1,10 +1,12 @@
 require "mind/version"
+require_relative "mind/pack/ping"
+require_relative "mind/super"
+require_relative "mind/super/constructor"
+require_relative "mind/super/item"
 require_relative "mind/constructor"
 require_relative "mind/item"
 
 module Mind
+  extend Pack::Ping
 
-  def self.ping
-    :pong
-  end
 end
